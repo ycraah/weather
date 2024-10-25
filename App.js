9de481213b747e592f5ddc06ffae4d0a4e429a8e
김예찬 <ycraah@gamil.com>
2024-10-25 20:43:41 +0900

    style 적용

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet } from 'react-native'
+import { Text } from 'react-native'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView>
+      <Text style={styles.text}>안녕하세요</Text>
+      <Text style={{ ...styles.text, color: 'blue' }}>안녕하세요!!!</Text>
+    </SafeAreaView>
+  )
 }
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 50,
+    color: 'red',
+    fontWeigth: 'bold',
+  },
+})
