5c9d04795a7b14eaee22c7fce1c22e729a4ce6f4
김예찬 <ycraah@gamil.com>
2024-10-26 10:09:42 +0900

    날씨앱 UI 완성

--- App.js
+++ App.js
@@ -1,17 +1,72 @@
-import { SafeAreaView, StatusBar } from 'react-native'
-import { TextInput } from 'react-native'
+import { View, StatusBar, StyleSheet, Text } from 'react-native'
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <StatusBar />
-      <TextInput
-        placeholder="이름을 입력하십시오"
-        onChangeText={(text) => console.log(text)}
-        onFocus={() => console.log('onFocus')}
-        onBlur={() => console.log('onBlur')}
-        onEndEditing={() => console.log('onEndEditing')}
-      />
-    </SafeAreaView>
+    <View style={styles.container}>
+      <View style={styles.cityCon}>
+        <Text style={styles.city}>Ansan</Text>
+      </View>
+      <View style={styles.weatherCon}>
+        <View style={styles.day}>
+          <Text style={styles.regDate}>10월 13일, 일, 13:18</Text>
+          <Text style={styles.desc}>맑음</Text>
+        </View>
+        <View style={styles.tempCon}>
+          <Text style={styles.temp}>24</Text>
+        </View>
+      </View>
+      <StatusBar style="auto" />
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffe01a',
+  },
+  cityCon: {
+    flex: 1,
+  },
+  city: {
+    flex: 1,
+    marginTop: 50,
+    paddingTop: 30,
+    fontSize: 40,
+    textAlign: 'center',
+    fontweight: 'bold',
+  },
+  weatherCon: {
+    flex: 3,
+  },
+  day: {
+    flex: 0.2,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  regDate: {
+    paddingTop: 10,
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingButton: 15,
+    backgroundColor: 'black',
+    color: 'white',
+    fontWieght: 'bold',
+    borderRadius: 20,
+    overflow: 'hidden',
+  },
+  desc: {
+    flex: 1.5,
+    marginTop: 20,
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+  tempCon: {
+    flex: 0.3,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  temp: {
+    fontSize: 120,
+  },
+})
