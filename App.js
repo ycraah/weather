d9d01dbc2adf388097e608c602bacf289bdb967c
김예찬 <ycraah@gamil.com>
2024-10-26 09:48:57 +0900

    텍스트 입력

--- App.js
+++ App.js
@@ -1,63 +1,17 @@
-import { SafeAreaView, ScrollView } from 'react-native'
-import { Text } from 'react-native'
+import { SafeAreaView, StatusBar } from 'react-native'
+import { TextInput } from 'react-native'
 
 export default function App() {
   return (
     <SafeAreaView>
-      <ScrollView>
-        <Text>
-          Id laboris elit cillum duis voluptate deserunt cupidatat. Ipsum aliqua
-          excepteur enim sit nostrud ad veniam fugiat fugiat in dolor
-          exercitation anim. Et irure irure ad adipisicing commodo eiusmod in
-          fugiat reprehenderit. Magna ex officia culpa sint consectetur laborum
-          eu pariatur. Fugiat tempor non incididunt esse magna voluptate irure
-          sunt voluptate qui quis proident commodo dolor. Est anim minim ex
-          voluptate fugiat aute excepteur velit magna Lorem irure. Eu sint ea
-          elit sit do ipsum mollit ut et deserunt deserunt aute mollit
-          reprehenderit. Pariatur mollit do officia dolor laboris aliquip.
-          Tempor id tempor nulla elit consectetur fugiat consequat consectetur.
-          Esse anim non elit anim aliquip ea et ea esse deserunt enim sunt. Qui
-          amet ullamco adipisicing proident cillum culpa sint veniam consequat
-          occaecat. Mollit cillum ex voluptate nulla incididunt. Aliquip irure
-          non irure aliquip occaecat cillum. Ea ipsum esse commodo nulla tempor.
-          Quis esse ut dolor aute cupidatat reprehenderit officia. Officia anim
-          eu irure commodo nulla. Laboris duis ad labore incididunt in commodo
-          voluptate aliquip. Eu voluptate eu Lorem nostrud officia amet pariatur
-          anim sit irure ipsum ea nostrud. Anim reprehenderit tempor magna
-          ullamco anim laboris nulla enim consectetur est pariatur quis. Enim ex
-          exercitation enim ex adipisicing ad ipsum et Lorem dolor nisi. Ex
-          magna officia incididunt culpa laborum. Eu pariatur ipsum laboris
-          pariatur aute proident proident irure ullamco qui. Lorem in anim
-          ullamco deserunt quis nostrud velit proident enim nulla quis ea
-          exercitation ad. Laboris nisi minim irure sint occaecat. Consequat
-          sunt dolore officia ut mollit occaecat mollit laborum Lorem anim ut
-          elit nulla. Minim amet fugiat in elit enim et. Dolor magna enim in
-          nulla deserunt ex ipsum. Sunt ex velit ut non duis nulla do eu nostrud
-          amet. Labore quis amet irure ullamco est duis incididunt velit dolore
-          velit elit magna magna exercitation. Proident ut qui elit nisi ullamco
-          ipsum. Cupidatat velit culpa mollit voluptate excepteur dolore ea. Et
-          sunt mollit est officia anim sunt cillum anim est laboris. Excepteur
-          elit nostrud culpa excepteur sint aliquip fugiat magna anim nostrud
-          magna. Mollit ullamco incididunt aliquip mollit. Incididunt
-          exercitation sunt amet laborum Lorem esse sunt ex ad occaecat aute.
-          Exercitation aute nostrud veniam duis aliqua minim quis deserunt esse
-          ipsum eiusmod. Irure cillum ex laboris fugiat duis consequat consequat
-          id qui. Ad ex veniam sit fugiat nulla qui mollit labore elit officia
-          sit. Non adipisicing dolor voluptate laboris laboris ullamco
-          adipisicing ex veniam ut. In ut irure laboris qui Lorem eu
-          reprehenderit aliqua commodo. Qui mollit ipsum mollit enim. Sunt
-          excepteur commodo minim nisi ullamco non aliqua. Magna proident ea
-          aliqua magna enim exercitation exercitation veniam. Ipsum occaecat
-          dolore id elit officia sunt dolor cupidatat sit fugiat laborum in
-          fugiat. Culpa officia ex tempor in elit exercitation eu consequat
-          magna. Nulla amet exercitation esse excepteur. In culpa sint minim
-          culpa consequat cupidatat officia et. Cillum pariatur irure Lorem
-          exercitation proident deserunt commodo esse reprehenderit est. Lorem
-          commodo consequat duis officia. Irure occaecat ea ex eiusmod et ut
-          fugiat mollit elit anim consequat qui. Esse dolor commodo velit
-          commodo sint aliqua.
-        </Text>
-      </ScrollView>
+      <StatusBar />
+      <TextInput
+        placeholder="이름을 입력하십시오"
+        onChangeText={(text) => console.log(text)}
+        onFocus={() => console.log('onFocus')}
+        onBlur={() => console.log('onBlur')}
+        onEndEditing={() => console.log('onEndEditing')}
+      />
     </SafeAreaView>
   )
 }
